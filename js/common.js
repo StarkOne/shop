6abbea8a464d508eff0adaae6f74a698382e78a2
Vlad <Stark.pro40@gmail.com>
2016-10-22 01:49:42 +0700

    окончательная версия верстки

--- js/common.js
+++ js/common.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 	//equalheight - одинаковая высота колонок
 	//Пример списка элементов:
 	//var eqElement = ".cat_container > div, .home_news > div"
-	var eqElement = ".h1_ite"
+	var eqElement = ".h1_ite,.sb_content>div"
 	$(window).load(function(){equalheight(eqElement);}).resize(function(){equalheight(eqElement);});
 
 	//Masked Input Plugin
